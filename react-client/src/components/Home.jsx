dd4326aec609b68c4e063550e71ea5e812704c87
Sieh Johnson <johnson_sieh@yahoo.com>
2018-09-10 22:04:51 -0500

    Implement dashboard, and set framework for future functionality. Checked dataflow with API.

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import Login from './LogIn.jsx';
 
 const Home = (props) => {
-  if (props.status) {
-    return (
-      <div>DASHBOARD</div>
-    )
-  } else {
-    return (
-      <div id="home">
-        <h1 className="welcome">Welcome to <em style={{ color: '#7bc57d' }}>The Green Bean</em> </h1>
-        <Login handleLogin={props.handleLogin} updateInfo={props.updateInfo}/>
+  return (
+    <div id="home">
+      <div className="divider"></div>
+      <div className="loginContainer">
+        <h1 className="welcome">Welcome to <em style={{ color: '#7bc57d' }}>The Green Bean</em> <img src="GB-blue.png" alt="logo" className="loginLogo" /> </h1>
+        <Login handleLogin={props.handleLogin} updateInfo={props.updateInfo} error={props.error} />
       </div>
-    )
-  }
+    </div>
+  )
+
 }
 
 export default Home;
--- react-client/src/components/LogIn.jsx
+++ react-client/src/components/LogIn.jsx
@@ -5,6 +5,7 @@ const Login = (props) => {
       <form className="form">
           <a className="username">Username: <input type="text" name="username" onChange={props.updateInfo}/> </a> 
           <a className="password">Password: <input type="password" name="password" onChange={props.updateInfo}/> </a> <br/>
+          {props.error ? <div className="err">Sorry, there was an error logging in. Please try again.</div> : ('') }
           <br/>
           <input type="button" value="Login" onClick={props.handleLogin}/>
       </form>
