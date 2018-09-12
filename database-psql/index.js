ea51afd33ab902f76947992749d77680c67c9373
Sieh Johnson <johnson_sieh@yahoo.com>
2018-09-11 19:06:03 -0500

    Implement login, logout, and sign up features. Save user to database and without duplicates. Dynamic/descriptive error message rendering.

--- database-psql/index.js
+++ database-psql/index.js
@@ -28,16 +28,54 @@ const selectAll = (callback) => {
 
 const insertOne = (itemObj, callback) => {
   const query = `INSERT INTO items (id, name, price, description) VALUES ($4, $1, $2, $3) ON CONFLICT (id) DO UPDATE SET price = $2;`;
-  const params = [itemObj.name, itemObj.price, itemObj.desc, itemObj.itemId]
+  const params = [itemObj.name, itemObj.price, itemObj.desc, itemObj.itemId];
   client.query(query, params, (err, data) => {
     if (err) {
-      console.log('error inserting into db')
+      console.log('error inserting into db');
       callback(err, null);
     }
     console.log('Added to db');
     callback(null, data);
-  })
-}
+  });
+};
+
+const addUser = (username, password, callback) => {
+  const query = `INSERT INTO users (name, password) VALUES ($1, $2);`;
+  const params = [username, password];
+  client.query(query, params, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+const findUser = (username, callback) => {
+  const query = `SELECT EXISTS (SELECT 1 FROM users WHERE name='${username}');`;
+
+  client.query(query, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data.rows[0].exists);
+    }
+  });
+};
+
+const checkPassword = (username, callback) => {
+  const query = `SELECT password FROM users WHERE name='${username}';`;
+  client.query(query, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data.rows[0].password);
+    }
+  });
+};
 
 module.exports.selectAll = selectAll;
 module.exports.insertOne = insertOne;
+module.exports.findUser = findUser;
+module.exports.addUser = addUser;
+module.exports.checkPassword = checkPassword;
