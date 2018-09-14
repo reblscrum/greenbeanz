75b740cf67e845a7722845c79c9afc60f8283344
Sieh Johnson <johnson_sieh@yahoo.com>
2018-09-14 17:58:23 -0500

    Implemented more css changes in the cart

--- react-client/src/components/Cart.jsx
+++ react-client/src/components/Cart.jsx
@@ -24,16 +24,19 @@ const Cart = (props) => {
           <div>
             {props.walmart.length > 0 ? (<h1 className="stores">Walmart</h1>) : ('')}
             <List className="walmart" items={props.walmart} addItem={props.addItem} showItems={props.showItems} showMore={props.showMore} showLess={props.showLess} />
-            {props.walmart.length > 0 ? <div id="show"> <em className="showMore" onClick={props.showMore} >Show More +</em> || <em className="showLess" onClick={props.showLess} >Show Less -</em></div> : ('') }
+            {props.walmart.length > 0 ? <div id="show"> <em className="showMore" onClick={props.showMore} >Show More + </em> || <em className="showLess" onClick={props.showLess} > Show Less -</em>
+              <div className="resLine">_________________________________________________</div> </div> : ('') }
           </div>
           <div>
             {props.heb.length > 0 ? (<h1 className="stores">HEB</h1>) : ('')}
             <List className="heb" items={props.heb} addItem={props.addItem} showItems={props.showItems} showMore={props.showMore} showLess={props.showLess} />
-            {props.heb.length > 0 ? <div id="show"> <em className="showMore" onClick={props.showMore} >Show More +</em> || <em className="showLess" onClick={props.showLess} >Show Less -</em></div> : ('') }</div>
+            {props.heb.length > 0 ? <div id="show"> <em className="showMore" onClick={props.showMore} >Show More + </em> || <em className="showLess" onClick={props.showLess} > Show Less -</em>
+              <div className="resLine">_________________________________________________</div></div> : ('') }</div>
           <div>
             {props.wholeFoods.length > 0 ? (<h1 className="stores">Whole Foods</h1>) : ('')}
             <List className="wholeFoods" items={props.wholeFoods} addItem={props.addItem} showItems={props.showItems} showMore={props.showMore} showLess={props.showLess} />
-            {props.wholeFoods.length > 0 ? <div id="show"> <em className="showMore" onClick={props.showMore} >Show More +</em> || <em className="showLess" onClick={props.showLess} >Show Less -</em></div> : ('') }</div>
+            {props.wholeFoods.length > 0 ? <div id="show"> <em className="showMore" onClick={props.showMore} >Show More + </em> || <em className="showLess" onClick={props.showLess} > Show Less -</em>
+              <div className="resLine">_________________________________________________</div></div> : ('') }</div>
         </div>
       </div>
     </div>
