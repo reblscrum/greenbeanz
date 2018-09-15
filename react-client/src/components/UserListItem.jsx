b71428931acbdc422fe71ed04a75738412ddd90f
Mitchel Constantin <mitchconstantin@gmail.com>
2018-09-15 15:39:18 -0500

    render rudementary table for each list

--- react-client/src/components/UserListItem.jsx
+++ react-client/src/components/UserListItem.jsx
@@ -7,10 +7,16 @@ class UserListItem extends Component {
   }
 
   render() {
+    console.log('here is the item: ');
+    console.log(this.props.item);
     return (
       <li className="usersListItem">
         <div> Name: {this.props.item.name} </div>
         <div>Price: {this.props.item.price} </div>
+        <div>Image: {this.props.item.image} </div>
+        <div>query: {this.props.item.query} </div>
+        <div> store_name: {this.props.item.store_name} </div>
+        <div>user_id: {this.props.item.user_id} </div>
       </li>
     );
   }
