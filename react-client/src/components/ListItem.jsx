bd8d052b82c2d76038442e2018e5469ffc6ad450
danielkang674 <danielkang674@gmail.com>
2018-09-16 10:31:29 -0500

    Got rid of extra

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const ListItem = (props) => (
-  <div className="indivRes" >
-  
-    <div><img src={props.item.image} /></div>
-    <a>{props.item.name}</a>
-    <div>PRICE: ${props.item.price}</div>
-    <input type="button" value="Add to list" name={`{"name": "${props.item.name}","price": "${props.item.price}", "image": "${props.item.image}", "store_name": "${props.store}","query": "${props.query}"}`} onClick={props.addItem} />
-  </div>
-); 
+const ListItem = (props) => {
+  let itemPrice = props.item.price;
+  if (typeof itemPrice === 'string') {
+    itemPrice = itemPrice.replace(/[^\d.]/gi, '');
+  }
+  return (
+    <div className="indivRes" >
+
+      <div><img src={props.item.image} /></div>
+      <a>{props.item.name}</a>
+      <div>PRICE: ${itemPrice}</div>
+      <input type="button" value="Add to list" name={`{"name": "${props.item.name}","price": "${props.item.price}", "image": "${props.item.image}", "store_name": "${props.store}","query": "${props.query}"}`} onClick={props.addItem} />
+    </div>
+  );
+};
 
 export default ListItem;
